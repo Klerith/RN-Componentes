232bb111585d072c3256ac9c500c0be1adc8c549
Fernando Herrera <fernando.herrera85@gmail.com>
2021-03-16 10:04:25 -0400

    antes de la tarea

--- src/screens/TextInputScreen.tsx
+++ src/screens/TextInputScreen.tsx
@@ -1,17 +1,73 @@
-import React from 'react'
-import { View, TextInput, StyleSheet } from 'react-native';
+import React, { useState } from 'react'
+import { View, TextInput, StyleSheet, KeyboardAvoidingView, Platform, ScrollView, TouchableWithoutFeedback, Keyboard, Text } from 'react-native';
+
 import { HeaderTitle } from '../components/HeaderTitle';
+import { CustomSwitch } from '../components/CustomSwitch';
 import { styles } from '../theme/appTheme';
+import { useForm } from '../hooks/useForm';
 
 export const TextInputScreen = () => {
-    return (
-        <View style={ styles.globalMargin }>
-            <HeaderTitle title="TextInputs" />
 
-            <TextInput 
-                style={ stylesScreen.inputStyle }
-            />
-        </View>
+    const { form, onChange, isSubscribed } = useForm({
+        name: '',
+        email: '',
+        phone: '',
+        isSubscribed: false
+    });
+
+    return (
+        <KeyboardAvoidingView
+            behavior={Platform.OS === "ios" ? "padding" : "height"}
+        >
+            <ScrollView>
+                <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+            
+                
+                    <View style={ styles.globalMargin }>
+                        <HeaderTitle title="TextInputs" />
+                    
+                        <TextInput 
+                            style={ stylesScreen.inputStyle }
+                            placeholder="Ingrese su nombre"
+                            autoCorrect={ false }
+                            autoCapitalize="words"
+                            onChangeText={ (value) => onChange( value, 'name' ) }
+                        />
+
+                        <TextInput 
+                            style={ stylesScreen.inputStyle }
+                            placeholder="Ingrese su email"
+                            autoCorrect={ false }
+                            autoCapitalize="none"
+                            onChangeText={ (value) => onChange( value, 'email' ) }
+                            keyboardType="email-address"
+                            keyboardAppearance="dark"
+                        />
+
+                        <View style={ stylesScreen.switchRow }>
+                            <Text style={ stylesScreen.switchText }>Suscribirse:</Text>
+                            <CustomSwitch isOn={ isSubscribed } onChange={ (value:any) => onChange( value, 'isSubscribed' ) } />
+                        </View>
+
+                        <HeaderTitle title={ JSON.stringify( form, null, 3 ) } />
+
+                        <HeaderTitle title={ JSON.stringify( form, null, 3 ) } />
+
+                        <TextInput 
+                            style={ stylesScreen.inputStyle }
+                            placeholder="Ingrese su teléfono"
+                            onChangeText={ (value) => onChange( value, 'phone' ) }
+                            keyboardType="phone-pad"
+                        />
+
+                        <View style={{ height: 100 }} />
+                    </View>
+
+                
+                </TouchableWithoutFeedback>
+
+            </ScrollView>
+        </KeyboardAvoidingView>
     )
 }
 
@@ -22,6 +78,18 @@ const stylesScreen = StyleSheet.create({
         borderColor: 'rgba(0,0,0,0.3)',
         height: 50,
         paddingHorizontal: 10,
-        borderRadius: 10
+        borderRadius: 10,
+        marginVertical: 10
+    },
+    switchRow:{
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginVertical: 10
+    },
+    switchText: {
+        fontSize: 25
     }
 });
+
+
